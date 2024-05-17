7f8a0c5342e85208f3f4c1953e231cc0e917840d
Caps <838269741@qq.com>
2024-05-17 23:14:31 +0800

    ci: add changesets

--- projects/project-a/src/index.ts
+++ projects/project-a/src/index.ts
@@ -1,5 +1,4 @@
-import { add1 } from '@friendlycode/tools';
+import { add1 } from "@friendlycode/tools";
 
-let a = add1(1, 2);
+let a = add1(1, 3);
 console.log(a);
-
---
+++ utils/tools/CHANGELOG.md
@@ -0,0 +1,13 @@
+# @friendlycode/tools
+
+## 0.0.4
+
+### Patch Changes
+
+- test ci build
+
+## 0.0.3
+
+### Patch Changes
+
+- test ci
